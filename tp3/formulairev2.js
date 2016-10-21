17a2a0aea6b8285b1fe6d543e2e1a87d8cf40283
latamene <latamene@users.noreply.github.com>
2016-10-21 16:22:22 +0200

    Update formulairev2.js

--- tp3/formulairev2.js
+++ tp3/formulairev2.js
@@ -1,5 +1,16 @@
 
 function validation(){
+   
+
+    var nom= document.getElementById("nom").value;
+
+    var prenom=document.getElementById("prenom").value;
+
+    var date=document.getElementById("date").value;
+
+    var adresse=document.getElementById("adresse").value;
+
+    var email=document.getElementById("email").value;
 
     
 
@@ -7,25 +18,7 @@ function validation(){
 
     
 
-    var Nom= document.getElementById("nom").value;
-
-    
-
-    
-
-    var Prenom=document.getElementById("Prenom").value;
-
-    var Date=document.getElementById("Date de naissance").value;
-
-    var Adresse=document.getElementById("Adrresse").value;
-
-    var Email=document.getElementById("Email").value;
-
-    
-
-    
-
-if(Nom == '' || Prenom=="" || Date de naissance=="" || Adresse=="" || Email=="") {
+    if(nom == '' || prenom=="" || date=="" || adresse=="" || email=="") {
 
         $(".modal-body").html("La saisie des champs est obligatoire")
 
@@ -33,18 +26,25 @@ if(Nom == '' || Prenom=="" || Date de naissance=="" || Adresse=="" || Email=="")
 
     }
 
-       else if ((Nom.length>=5)&&(Prenom.length>=5)&& (Date.length>=5)&&(Adresse.length>=5)&& (Email.length>=5)){        
+       else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
 
-            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284"/>');
+            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>');
+
+            $('.modal').modal('show');
 
        }
 
     else{
 
-        $(".modal-body").html("veuillez remplir tous les champs svp!")
+        $(".modal-body").html("veuillez remplir tous les champs svp!");
 
         $('.modal').modal('show');
 
     }
 
 }
+
+
+
+
+
