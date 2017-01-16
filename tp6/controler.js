19cbaf520296d3e259ea4cac73cc01302e9d2e65
latamene <latamene@users.noreply.github.com>
2017-01-16 11:13:21 +0100

    Update controler.js

--- tp6/controler.js
+++ tp6/controler.js
@@ -4,7 +4,26 @@ meteoControllers.controller('MainController', ['$scope', '$http',
    function($scope, $http) {
 $scope.recherche = function() {
 /* appel AJAX à l’API openweathermap */
-$http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid=[e0c5699f772b215c86af11bd1b7a41d1]
+$http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid='e0c5699f772b215c86af11bd1b7a41d1'
+)
+  .success(function(data) {
+/* on met dans l’objet 
+meteo
+ les données retournées
+par openweathermap */
+ $scope.meteo = data;
+ }).error(function(data) {
+/* en cas d’erreur */
+ $scope.errorMsg =  "Hum. Error... please retry.";
+ });
+}
+
+$scope.gps = function() {
+   
+   // js geoloc
+   
+/* appel AJAX à l’API openweathermap */
+$http.get('https://demo.bilelz.fr/owmap/?q='+$scope.city+'&units=metric&appid='e0c5699f772b215c86af11bd1b7a41d1'
 )
   .success(function(data) {
 /* on met dans l’objet 
