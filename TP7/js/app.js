ea904f3d8b6b3ecf0a66b6dbc9cd3aa52de93ec1
latamene <latamene@users.noreply.github.com>
2017-01-30 15:54:26 +0100

    Update app.js

--- TP7/js/app.js
+++ TP7/js/app.js
@@ -1,2 +1,4 @@
 /* App Module */
 var meteoApp = angular.module('meteoApp', ['ngRoute', 'meteoControllers']);
+/* App Module */
+var meteoApp = angular.module('meteoApp', ['ngRoute', 'meteoControllers', 'ngMessages']);
